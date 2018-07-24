f2133fe24015ec85003b9297f9b528c92c42585a
ANKITA DUTTA <38917649+Ankitadutta2@users.noreply.github.com>
2018-07-24 01:00:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,15 @@ $(document).ready(function() {
 	});
 
 });
+var typed = new Typed(".typed", {
+		strings: ["UX Researcher.", "Data Analyst.", "Business Analyst."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
 
+});
 $("[data-fancybox]").fancybox();
 
 
