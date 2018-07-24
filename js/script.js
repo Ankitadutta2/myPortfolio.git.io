ca171043a3691582576279cf925555d195bf12cf
ANKITA DUTTA <38917649+Ankitadutta2@users.noreply.github.com>
2018-07-24 10:38:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -58,22 +58,5 @@ $("#navigation li a").click(function(e) {
 
 
 
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
-
-	$(window).on("scroll", stickyNavigation);
-
-	function stickyNavigation() {
-
-		var body = $("body");
-
-		if($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px");
-			body.addClass("fixedNav");
-		}
-		else {
-			body.css("padding-top", 0);
-			body.removeClass("fixedNav");
-		}
-
+	
 
